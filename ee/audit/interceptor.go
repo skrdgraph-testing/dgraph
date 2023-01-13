19db4a013de168574862002b3128d65b874a2468
Anurag <anurags92@gmail.com>
2023-01-13 15:32:59 +0530

    Address comments

--- ee/audit/interceptor.go
+++ ee/audit/interceptor.go
@@ -1,7 +1,8 @@
+//go:build oss
 // +build oss
 
 /*
- * Copyright 2022 Dgraph Labs, Inc. and Contributors
+ * Copyright 2023 Dgraph Labs, Inc. and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,9 +23,9 @@ import (
 	"context"
 	"net/http"
 
-	"google.golang.org/grpc"
-
 	"github.com/dgraph-io/dgraph/graphql/schema"
+
+	"google.golang.org/grpc"
 )
 
 func AuditRequestGRPC(ctx context.Context, req interface{},
